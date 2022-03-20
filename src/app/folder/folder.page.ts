2a0c941b765491e025fae64121d339ddd1bc210e
Faye24 <fayemarieoscobido@gmail.com>
2022-03-20 11:43:20 +0800

    IonicApplication

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ActionSheetController } from "@ionic/angular";
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +10,62 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
-
-  constructor(private activatedRoute: ActivatedRoute) { }
-
+  public userList = [
+    {
+      id: 1,
+      name: 'John'
+    },
+    {
+      id: 2,
+      name: 'Joe'
+    },
+    {
+      id: 3,
+      name: 'Eric'
+    }
+  ]
+  
+  constructor(private activatedRoute: ActivatedRoute, public actionsheetCtrl: ActionSheetController, public alertCtrl: AlertController) {}
+  
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
-}
+  async showactionButtons(index: number) {
+    let actionSheet = this.actionsheetCtrl.create({
+      header: 'Select',
+      cssClass: 'button-css',
+      animated: true,
+      backdropDismiss: true,
+      keyboardClose: false,
+
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.userList.splice(index, 1); 
+          this.showAlert();
+        }
+      },
+       {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+        }
+        }]
+    });
+    (await actionSheet).present();
+  }
+    async showAlert(){
+    const alert = this.alertCtrl.create({
+      header: 'Successfully Deleted!',
+      buttons: ['OK']
+    });
+    (await alert).present();
+  }
+
+
+  }
